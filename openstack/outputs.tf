26efba369745972ca2aa3174d0ad4ce6a9259ddd
Artem-Petroshenko <90929207+Artem-Petroshenko@users.noreply.github.com>
2026-01-14 16:16:27 +0300

    Update outputs.tf

--- openstack/outputs.tf
+++ openstack/outputs.tf
@@ -1,4 +1,4 @@
-output "elk_vm_ip" {
+output "Petroshenko1_ip" {
   description = "IP-адрес ВМ в сети sutdents-net"
-  value       = openstack_compute_instance_v2.elk_vm.network[0].fixed_ip_v4
+  value       = openstack_compute_instance_v2.Petroshenko1.network[0].fixed_ip_v4
 }
